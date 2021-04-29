d7adfcd23a325611d44d868b27ac601e3ae7416a
Edqe14 <configxgt@gmail.com>
2021-04-29 18:12:20 +0700

    pwa 100

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,10 +22,19 @@ export default function Home(): unknown {
         />
 
         <link rel='manifest' href='manifest.json' />
+        <link rel='apple-touch-icon' href='/icons/apple-icon-180.png' />
         <meta charSet='UTF-8' />
         <meta http-equiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta name='apple-touch-icon' content='/icons/apple-icon-180.png' />
+        <meta name='theme-color' content='#2196f3' />
+        <meta name='og:title' content='HoloGrem' />
+        <meta name='og:type' content='website' />
+        <meta name='og:image' content='/images/logo.png' />
+        <meta property='og:locale' content='en_US' />
+        <meta
+          name='og:description'
+          content='A Collection of Hololive Gremlins'
+        />
       </Head>
     </>
   );
